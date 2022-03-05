16f3eb1cab1f6c770e93b5b44ca5d4abfa3e9e09
Pmnmpr <pmnmpr@gmail.com>
2022-03-05 20:10:36 +0000

    Obfuscated

---
+++ src/Pmpr/Package/WoocommerceVariation/WoocommerceVariation.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             6223c38f5ce38             |
+    |_______________________________________|
+*/
+ namespace Pmpr\Package\WoocommerceVariation; use Pmpr\Common\Foundation\Decorator\Plugin\DecoratorWoocommerce; class WoocommerceVariation extends Container { public function __construct() { $this->gkieogwukagigisy(__DIR__); parent::__construct(); } public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\141\x66\164\145\x72\x5f\x73\145\164\165\160\x5f\x74\x68\145\155\145", [$this, "\145\x6e\161\x75\145\165\x65"]); } public function load() { if (!DecoratorWoocommerce::kmymkocwcawgeccm()) { goto cecuyayqoioasumi; } Product::symcgieuakksimmu(); $this->enqueue(); cecuyayqoioasumi: } public function enqueue() { $eygsasmqycagyayw = $this->miocmcoykayoyyau(); $eygsasmqycagyayw->oeoquuwkoywiuesy($eygsasmqycagyayw->owygwqwawqoiusis("\x76\x61\162\151\x61\x74\x69\x6f\156", $eygsasmqycagyayw->get("\146\x72\x6f\156\x74\x2e\152\x73"))->simswskycwagoeqy()); } }
