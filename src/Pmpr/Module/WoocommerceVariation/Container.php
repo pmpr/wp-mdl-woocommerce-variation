21dc87cbda905af96415cc32ed67c660e311582a
Pmnmpr <pmnmpr@gmail.com>
2022-04-07 10:00:03 +0000

    Obfuscated

--- src/Pmpr/Module/WoocommerceVariation/Container.php
+++ src/Pmpr/Module/WoocommerceVariation/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             624c11a47e045             |
+    |             624eb5fc8465f             |
     |_______________________________________|
 */
  namespace Pmpr\Module\WoocommerceVariation; use Pmpr\Common\Foundation\Container\Container as BaseClass; class Container extends BaseClass { }
--- src/Pmpr/Module/WoocommerceVariation/WoocommerceVariation.php
+++ src/Pmpr/Module/WoocommerceVariation/WoocommerceVariation.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             624c11a47e045             |
+    |             624eb5fc8465f             |
     |_______________________________________|
 */
- namespace Pmpr\Module\WoocommerceVariation; use Pmpr\Common\Foundation\Decorator\Plugin\DecoratorWoocommerce; class WoocommerceVariation extends Container { public function __construct() { $this->gkieogwukagigisy(__DIR__); parent::__construct(); } public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\141\x66\164\x65\x72\137\x73\145\x74\x75\160\x5f\x74\x68\x65\155\145", [$this, "\x65\x6e\x71\x75\x65\165\145"]); } public function load() { if (!DecoratorWoocommerce::kmymkocwcawgeccm()) { goto cecuyayqoioasumi; } Product::symcgieuakksimmu(); $this->enqueue(); cecuyayqoioasumi: } public function enqueue() { $eygsasmqycagyayw = $this->miocmcoykayoyyau(); $eygsasmqycagyayw->oeoquuwkoywiuesy($eygsasmqycagyayw->owygwqwawqoiusis("\x76\x61\x72\x69\x61\x74\x69\157\x6e", $eygsasmqycagyayw->get("\x66\162\157\156\164\x2e\x6a\x73"))->simswskycwagoeqy()); } }
+ namespace Pmpr\Module\WoocommerceVariation; use Pmpr\Common\Foundation\Decorator\Plugin\DecoratorWoocommerce; class WoocommerceVariation extends Container { public function __construct() { $this->gkieogwukagigisy(__DIR__); parent::__construct(); } public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\141\146\164\145\162\137\163\x65\164\165\160\x5f\164\x68\x65\x6d\x65", [$this, "\145\x6e\161\165\145\165\x65"]); } public function load() { if (!DecoratorWoocommerce::kmymkocwcawgeccm()) { goto cecuyayqoioasumi; } Product::symcgieuakksimmu(); $this->enqueue(); cecuyayqoioasumi: } public function enqueue() { $eygsasmqycagyayw = $this->miocmcoykayoyyau(); $eygsasmqycagyayw->oeoquuwkoywiuesy($eygsasmqycagyayw->owygwqwawqoiusis("\x76\141\x72\x69\141\164\151\157\x6e", $eygsasmqycagyayw->get("\x66\x72\157\156\164\56\152\163"))->simswskycwagoeqy()); } }
